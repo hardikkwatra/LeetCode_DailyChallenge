f9422d3f715ea471740ebb33b37fce5f21b58422
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-07-14 06:08:31 +0530

    Time: 3 ms (100.00%), Space: 56.4 MB (64.97%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.java
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.java
@@ -1,14 +1,23 @@
 class Solution {
     public int longestSubsequence(int[] arr, int difference) {
-        Map<Integer, Integer> dp = new HashMap<>();
-        int answer = 1;
-        
-        for (int a : arr) {
-            int beforeA = dp.getOrDefault(a - difference, 0);
-            dp.put(a, beforeA + 1);
-            answer = Math.max(answer, dp.get(a));
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
         }
-        
-        return answer;
+        int[] dp = new int[max - min + 1];
+        max = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            int cur = arr[i] - min, pred = cur - difference;
+            if (pred >= 0 && pred < dp.length) {
+                dp[cur] = 1 + dp[pred];
+                if (dp[cur] > max) max = dp[cur];
+            } else {
+                dp[cur] = 1;
+            }
+        }
+        return max;
     }
 }
