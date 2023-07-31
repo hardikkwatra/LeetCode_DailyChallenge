630dac1533912afa21fa87f739c62e2e90126bb8
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-08-01 01:32:56 +0530

    Time: 3 ms (42.77%), Space: 45 MB (14.79%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -5,13 +5,14 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == 1){
                 count++;
+                ans = Math.max(ans,count);
             }
             if(nums[i] != 1){
-                ans = Math.max(ans,count);
+                
                 count = 0;
             }
         }
-        ans = Math.max(ans,count);
+        
         return ans;
     }
 }
