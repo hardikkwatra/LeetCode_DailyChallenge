535df1af70665091f918b17968eccf39dd1e3c01
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-09-28 06:20:47 +0530

    Time: 1 ms (81.28%), Space: 44.2 MB (42.04%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,11 +1,31 @@
 class Solution {
      public int[] sortArrayByParity(int[] A) {
-        for (int i = 0, j = 0; j < A.length; j++)
-            if (A[j] % 2 == 0) {
-                int tmp = A[i];
-                A[i++] = A[j];
-                A[j] = tmp;;
+        int left = 0;
+        int right = A.length - 1;
+        
+        while (left < right) {
+            if (A[left] % 2 == 1 && A[right] % 2 == 0) {
+                // Swap odd and even numbers
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                left++;
+                right--;
+            } else if (A[left] % 2 == 0) {
+                left++;
+            } else if (A[right] % 2 == 1) {
+                right--;
             }
+        }
+        
         return A;
     }
-}
+
+ }
+
+
+
+
+
+
+
