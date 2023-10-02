25d0d8f3d0b3417e492c2a6b83cba090af761a0e
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-10-03 00:55:16 +0530

    Time: 14 ms (69.83%), Space: 44.2 MB (56.20%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,18 +1,26 @@
-class Solution {
+public class Solution {
     public boolean winnerOfGame(String colors) {
-        int alice = 0;
-        int bob = 0;
+        int alice_plays = 0, bob_plays = 0, count = 0;
         
-        for (int i = 1; i < colors.length() - 1; i++) {
-            if (colors.charAt(i - 1) == colors.charAt(i) && colors.charAt(i) == colors.charAt(i + 1)) {
-                if (colors.charAt(i) == 'A') {
-                    alice++;
+        for (int i = 1; i < colors.length(); i++) {
+            if (colors.charAt(i) == colors.charAt(i - 1)) {
+                count++;
+            } else {
+                if (colors.charAt(i - 1) == 'A') {
+                    alice_plays += Math.max(0, count - 1);
                 } else {
-                    bob++;
+                    bob_plays += Math.max(0, count - 1);
                 }
+                count = 0;
             }
         }
+
+        if (colors.charAt(colors.length() - 1) == 'A') {
+            alice_plays += Math.max(0, count - 1);
+        } else {
+            bob_plays += Math.max(0, count - 1);
+        }
         
-        return alice - bob >= 1;
+        return alice_plays > bob_plays;
     }
 }
