3b0bb532c836d94db71daed74b5b1e76fb0662b4
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-10-03 11:33:03 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (85.07%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -3,14 +3,11 @@ class Solution {
         int[] count = new int[101];
         int ans = 0;
         
-        for(int i = 0; i < nums.length; i++){
-                count[nums[i]-1]++;
+        for (int i = 0; i < nums.length; i++) {
+            ans += count[nums[i] - 1];
+            count[nums[i] - 1]++;
         }
-        for(int i = 0; i < count.length; i++){
-            if(count[i] >= 1){
-                ans = ans + (count[i] * (count[i] - 1)) / 2;
-            }
-        }
+
         return ans;
     }
 }
