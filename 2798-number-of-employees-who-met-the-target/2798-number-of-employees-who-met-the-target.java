eb69524fe0c231faf34caba28dfb0c229fe3a8e8
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-10-12 17:00:18 +0530

    Time: 6 ms (46.35%), Space: 40.7 MB (95.58%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -1,12 +1,9 @@
 class Solution {
-    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-    int count = 0;
-    for (int hour : hours) {
-        if (hour >= target) {
-            count++;
-        }
-    }
-    return count;
-  }
+
+public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+    return (int) Arrays.stream(hours)
+        .filter(hour -> hour >= target)
+        .count();
+}
 
 }
