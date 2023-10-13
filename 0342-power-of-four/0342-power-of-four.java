cf457650755bdf99cab20bec37b9b3afed00faaa
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-10-13 20:41:20 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (14.59%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1 || n==4)
-            return true;
-        if(n%4!=0 || n==0)
-            return false;
-        return isPowerOfFour(n/4);
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
     }
 }
