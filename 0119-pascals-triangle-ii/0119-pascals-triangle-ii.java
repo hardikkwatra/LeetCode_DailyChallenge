ef1a8d57de5e5abc2d921d48afca297f14c818f9
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-10-16 19:07:49 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (78.48%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,13 +1,14 @@
-public class Solution {
-    public List<Integer> getRow(int k) {
-        Integer[] arr = new Integer[k + 1];
-        Arrays.fill(arr, 0);
-        arr[0] = 1;
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long ans = 1;
+
+        for(int i = 0;i<=rowIndex;i++){
+            row.add((int)ans);
+            ans = ans*(rowIndex-i);
+            ans = ans/(i+1);
+        }
         
-        for (int i = 1; i <= k; i++) 
-            for (int j = i; j > 0; j--) 
-                arr[j] = arr[j] + arr[j - 1];
-        
-        return Arrays.asList(arr);
+        return row;
     }
 }
