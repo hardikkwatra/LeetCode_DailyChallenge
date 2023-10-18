3846e6d44cecf718cd8f04275292266782c8e6a2
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-10-19 04:08:48 +0530

    Time: 3 ms (90.38%), Space: 41.6 MB (94.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,8 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
         int[] alpha = new int[26];
         for (int i = 0; i < s.length(); i++) alpha[s.charAt(i) - 'a']++;
         for (int i = 0; i < t.length(); i++) alpha[t.charAt(i) - 'a']--;
