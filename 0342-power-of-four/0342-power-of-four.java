faa392a6d20d1909295b90381659e679ba4bb426
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-10-23 17:20:05 +0530

    Time: 1 ms (76.31%), Space: 39.4 MB (58.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,10 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        
-        double logBase4 = Math.log(n) / Math.log(4);
-        return Math.abs(logBase4 - Math.round(logBase4)) < 1e-10;
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
     }
 }
