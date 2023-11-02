7d16636f031293e9f758502e2a9e3662a41d6814
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-11-02 17:47:04 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (69.65%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -1,29 +1,23 @@
 class Solution {
-    int count = 0;
-    
-    Pair<Integer, Integer> postOrder(TreeNode root) {
-        if (root == null) {
-            return new Pair(0, 0);
-        }
-        
-        // First iterate over left and right subtrees.
-        Pair<Integer, Integer> left = postOrder(root.left);
-        Pair<Integer, Integer> right = postOrder(root.right);
-        
-        int nodeSum = left.getKey() + right.getKey() + root.val;
-        int nodeCount = left.getValue() + right.getValue() + 1;
+    private int count;
 
-        // Check if the average of the subtree is equal to the node value.
-        if (root.val == nodeSum / (nodeCount)) {
+    public int averageOfSubtree(TreeNode root) {
+        count = 0;
+        process(root);
+        return count;   
+    }
+
+    public int[] process(TreeNode node) {
+        if (node == null) return new int[2];
+        int[] prev = process(node.left);
+        int[] next = process(node.right);
+        int subCount = prev[0] + next[0] + 1;
+        int subSum = prev[1] + next[1] + node.val;
+        if (subSum / subCount == node.val) {
             count++;
         }
-        
-        // Return the sum of nodes and the count in the subtree.
-        return new Pair(nodeSum, nodeCount);
-    }
-    
-    public int averageOfSubtree(TreeNode root) {
-        postOrder(root);
-        return count;
+        prev[0] += next[0] + 1;
+        prev[1] += next[1] + node.val;
+        return prev;
     }
 }
