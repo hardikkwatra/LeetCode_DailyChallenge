94a1e71fc90626e6fc030bfd41ec9c4c5f504c59
Hardik Kwatra <78718601+hardikkwatra@users.noreply.github.com>
2023-11-18 21:08:45 +0530

    Time: 11 ms (99.57%), Space: 57.6 MB (34.33%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,43 +1,42 @@
+        
+        
 class Solution {
-    public int check(int i, int k, int[] nums, long[] prefix) {
-        int target = nums[i];
-        int left = 0;
-        int right = i;
-        int best = i;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            long count = i - mid + 1;
-            long finalSum = count * target;
-            long originalSum = prefix[i] - prefix[mid] + nums[mid];
-            long operationsRequired = finalSum - originalSum;
-            
-            if (operationsRequired > k) {
-                left = mid + 1;
-            } else {
-                best = mid;
-                right = mid - 1;
-            }
-        }
-        
-        return i - best + 1;
-    }
-    
     public int maxFrequency(int[] nums, int k) {
-        Arrays.sort(nums);
-        long[] prefix = new long[nums.length];
-        prefix[0] = nums[0];
-        
-        for (int i = 1; i < nums.length; i++) {
-            prefix[i] = nums[i] + prefix[i - 1];
-        }
-        
-        int ans = 0;
+        countingSort(nums);
+        int start = 0;
+        int preSum = 0;
+        int total = 1;
         for (int i = 0; i < nums.length; i++) {
-            ans = Math.max(ans, check(i, k, nums, prefix));
+            int length = i - start + 1;
+            int product = nums[i] * length;
+            preSum += nums[i];            
+            while (product - preSum > k) {
+                preSum -= nums[start++];
+                length--;
+                product = nums[i] * length;
+            }
+            total = Math.max(total, length);
         }
-        
-        return ans;
-        
+      
+        return total;
+    }
+    private void countingSort(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int num: nums) {
+            max = Math.max(max, num);
+        }
+        int[] map = new int[max + 1];
+        for(int num: nums) {
+            map[num]++;
+        }
+        int i = 0;
+        int j = 0;
+        while(i <= max) {
+            if (map[i]-- > 0) {
+                nums[j++] = i;
+            } else {
+                i++;
+            }
+        }        
     }
 }
